ae213a2056c7d63bf43110f5df4b8513687f1260
Devanshi Sinha <devanshisinha37@gmail.com>
2021-10-21 23:27:10 +0530

    Implement proof of work

--- block.js
+++ block.js
@@ -14,14 +14,31 @@ class Block {
   }
 
   static mineBlock ({ lastBlock, data }) {
-    const timestamp = Date.now()
     const lastHash = lastBlock.lastHash
-    return new this({
-      timestamp,
-      lastHash,
-      hash: cryptoHash(timestamp, lastHash, data),
-      data
-    })
+    let { difficulty } = lastBlock
+    let hash, timestamp
+    let nonce = 0
+
+    do {
+      nonce++
+      timestamp = Date.now()
+      difficulty = Block.adjustDifficulty({
+        originalBlock: lastBlock,
+        timestamp
+      })
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
+    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty))
+
+    return new this({ timestamp, lastHash, data, difficulty, nonce, hash })
+  }
+
+  static adjustDifficulty ({ originalBlock, timestamp }) {
+    const { difficulty } = originalBlock
+    if (difficulty < 1) return 1
+    if (timestamp - originalBlock.timestamp > MINE_RATE) {
+      return difficulty - 1
+    }
+    return difficulty + 1
   }
 }
 
--- config.js
+++ config.js
@@ -1,8 +1,13 @@
+const MINE_RATE = 1000 // milliseconds
+const INITIAL_DIFFICULTY = 3
+
 const GENESIS_DATA = {
     timestamp: 1,
     lastHash: '-----',
     hash: 'genesis-hash',
+    difficulty: INITIAL_DIFFICULTY,
+    nonce: 0,
     data: []
   }
 
-module.export = { GENESIS_DATA }
+module.export = { GENESIS_DATA, MINE_RATE }
